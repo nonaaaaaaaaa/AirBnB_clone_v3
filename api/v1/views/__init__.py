836b3bfd88f73764ed5c62a5567c58c14c93f215
nonaaaaaaaaa <nehalserag44@gmail.com>
2024-04-28 06:57:18 -0700

    l

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import views to register them with the Blueprint
 from api.v1.views.index import *
+
+from api.v1.views.states import *
+
+from api.v1.views.cities import *
